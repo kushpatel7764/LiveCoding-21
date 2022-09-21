9a7b549426ff9c3726512ba2af2d10d8dee9c62e
kushpatel7764 <kushpatelrp1234@gmail.com>
2022-09-21 13:03:17 -0400

    commit 1

---
+++ src/DemoLinkedList.java
@@ -0,0 +1,6 @@
+public class DemoLinkedList {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.printList();
+    }
+}
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,30 @@
+public class SinglyLinkedList {
+    int size;
+    Node head;
+    Node tail;
+
+    public SinglyLinkedList(){
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    public void clear (){
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    public void printList(){
+        if(size == 0){
+            System.out.println("list is empty");
+            return;
+        }
+
+        Node current = head;
+        while (current != null){
+
+        }
+    }
+
+}
