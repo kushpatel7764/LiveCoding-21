2d88a6a8137280e6396b97b073a3861105804da8
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-09-26 13:44:55 -0400

    Update SinglyLinkedList.java

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,3 +1,6 @@
+import java.util.NoSuchElementException;
+
+
 public class SinglyLinkedList {
     int size;
     Node head;
@@ -30,6 +33,23 @@ public class SinglyLinkedList {
         System.out.print("end");
     }
 
+    public Node getFirst(){
+        if (size ==0){
+            System.out.println("This list is empty");
+            return null; 
+        } else{
+            return head;
+        }
+    }
+    public Node getLast(){
+        if (size ==0){
+            System.out.println("This list is empty");
+            return null; 
+        } else{
+            return tail;
+        }
+    }
+
     public void addFirst(String data){
         if(size == 0){
             Node newNode = new Node(data, null);
@@ -55,4 +75,52 @@ public class SinglyLinkedList {
             ++size;
         }
     }
+    public void removeFirst(){
+        if (size == 0 ){
+            System.out.println("This list is empty");
+            } else {
+                head = head.next;
+                --size; 
+            }
+    }
+
+    public void removeLast(){
+        if (size == 0 ){
+        System.out.println("This list is empty");
+        } else {
+            Node current = head;
+        while(current.next != tail){
+            current = current.next;
+        }
+    
+        current.next = null;
+        --size;
+        tail = current;
+    } 
+    }
+
+    public Node get(int index){
+        if(index > size -1 || index < 0){
+            throw new NoSuchElementException("This");
+        }
+        if(index == 0){
+            getFirst(); 
+        }
+        else if (index == size-1){
+            getLast();
+        }
+        if (size == 0 ){
+        throw new NoSuchElementException("This list is empty");
+        } else {
+            Node current = head;
+         
+                for(int i = 0; i != index; i++ ){
+                   
+                    current = current.next; 
+                    }
+            return current;
+        }
+    }
+
+
 }
