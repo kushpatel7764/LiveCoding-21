e1c5b5c8914dd434f546bb2e532129e9ac416e41
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-09-28 13:42:23 -0400

    Update DemoLinkedList.java

--- src/DemoLinkedList.java
+++ src/DemoLinkedList.java
@@ -1,12 +1,20 @@
 public class DemoLinkedList {
     public static void main(String[] args) {
-        SinglyLinkedList list = new SinglyLinkedList();
-        list.printList();
-
-        list.addFirst("2");
-        list.addLast("1");
+        SinglyLinkedList list = new SinglyLinkedList(); 
+        list.addFirst("1");
+        list.addLast("3");
+        list.add(1, "2");
+       // list.add(1, "4");
+        //list.add(4, "5");
+        //list.add(4, "7");
+        // list.add(5, "6");
+        // list.add(1, "1");
+        // list.add(1, "2");
         System.out.println(list.get(0).data);
         System.out.println(list.get(1).data);
+        list.printList();
+        list.remove(2);
+        System.out.println();
         list.printList();
     }
 }
